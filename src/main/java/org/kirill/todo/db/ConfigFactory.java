0b270e008a95e473c51c26a8e6cd9d17a58759ea
kirill <semenovkirill.spb@gmail.com>
2016-03-22 08:39:11 +0300

    db start

--- src/main/java/org/kirill/todo/db/ConfigFactory.java
+++ src/main/java/org/kirill/todo/db/ConfigFactory.java
@@ -2,6 +2,7 @@ package org.kirill.todo.db;
 
 import io.vertx.core.json.JsonObject;
 
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 /**
@@ -9,25 +10,31 @@ import java.util.ResourceBundle;
  */
 public class ConfigFactory {
     public static JsonObject getConfig() {
-        ResourceBundle bundle = ResourceBundle.getBundle("local");
-        if (bundle == null) {
+        try {
+            ResourceBundle bundle = ResourceBundle.getBundle("local");
+            return new JsonObject()
+                    .put("host", bundle.getString("host"))
+                    .put("username", bundle.getString("username"))
+                    .put("password", bundle.getString("password"))
+                    .put("database", bundle.getString("database"));
+        } catch (MissingResourceException ignored) {
             String concatenated = System.getenv("heroku");
             if (concatenated != null) {
                 String[] split = concatenated.split(";");
                 return new JsonObject()
-                    .put("host", split[0])
-                    .put("username", split[1])
-                    .put("password", split[2])
-                    .put("database", split[3]);
+                        .put("host", split[0])
+                        .put("username", split[1])
+                        .put("password", split[2])
+                        .put("database", split[3]);
+            } else {
+                ResourceBundle bundle = ResourceBundle.getBundle("test");
+                return new JsonObject()
+                    .put("host", bundle.getString("host"))
+                    .put("username", bundle.getString("username"))
+                    .put("password", bundle.getString("password"))
+                    .put("database", bundle.getString("database"));
+
             }
         }
-        if (bundle == null) {
-            bundle = ResourceBundle.getBundle("test");
-        }
-        return new JsonObject()
-                .put("host", bundle.getString("host"))
-                .put("username", bundle.getString("username"))
-                .put("password", bundle.getString("password"))
-                .put("database", bundle.getString("database"));
     }
 }
